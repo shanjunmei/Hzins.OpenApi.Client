e4bb736d79849ea4768fe533378fe9893dabf142
hz15101769 <445052471@qq.com>
2016-03-21 13:46:32 +0800

    移除空白行

--- Hzins.OpenApi.Client/openapi/Configure.cs
+++ Hzins.OpenApi.Client/openapi/Configure.cs
@@ -12,38 +12,35 @@ using System;
 
 namespace com.hzins.openapi.client
 {
-    /// <p>
-    /// 
-    /// 
-    /// 
-    /// </p>
-    public class Configure
-    {
-        public class Channel
-        {
-            /// 22803
-            public static int? partnerId = null;
-            /// 22803^*#%
-            public static String channelKey = null;
+   /// <p>
+   /// 
+   /// 
+   /// 
+   /// </p>
+   public class Configure
+   {
+      public class Channel
+      {
+         /// 22803
+         public static int? partnerId = null;
+         /// 22803^*#%
+         public static String channelKey = null;
 
-        }
+      }
 
-        public class Request
-        {
-            public static String baseUrl = "http://channel.hzins.com/api/";
-            ///  ms为单位
-            public static int http_connection_timeout = 0;
-            ///  默认为0，系统默认超时时间，根据操作系统而定
-            ///  ms为单位
-            public static int http_read_timeout = 0;
-            ///  默认为0，系统默认超时时间，根据操作系统而定
-            public const String userAgent = "Channel OpenApi Sdk(v1.1)";
+      public class Request
+      {
+         public static String baseUrl = "http://channel.hzins.com/api/";
+         ///  ms为单位
+         public static int http_connection_timeout = 0;
+         ///  默认为0，系统默认超时时间，根据操作系统而定
+         ///  ms为单位
+         public static int http_read_timeout = 0;
+         ///  默认为0，系统默认超时时间，根据操作系统而定
+         public const String userAgent = "Channel OpenApi Sdk(v1.1)";
+         public const String contentType = "application/json;charset=UTF-8";
 
-            public const String contentType = "application/json;charset=UTF-8";
+      }
 
-
-
-
-        }
-    }
+   }
 }
